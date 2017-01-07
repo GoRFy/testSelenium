2b80c6fcb70f504f0e0684c11a8d3e18e3a46abc
GoRFy <lambot.rom@gmail.com>
2017-01-07 12:18:10 +0100

    Subscribe test

--- Selenium.php
+++ Selenium.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'vendor/autoload.php';
-
+require_once 'helpers.php';
 
 class SeleniumTest extends PHPUnit_Extensions_Selenium2TestCase
 {
@@ -20,13 +20,39 @@ class SeleniumTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->assertEquals('SPORT NATION | WORLD WIDE', $this->title());
     }*/
 
-    public function testValidFormSubscription()
+    public function testFormInvalidEmail()
+    {
+        $this->url('http://romainlambot.fr/ninjav2/user/subscribe');
+        $this->byId('email')->value('lambot.rom@gmail.com');
+        $this->byId('username')->value('TestingRootBruh');
+        $this->byCssSelector('.form_subscription')->submit();
+        $text = $this->byId("errorMsg")->text();
+        $this->assertEquals("Email incorrect ou deja existante", $text, "Email incorrect ou deja existante");
+        sleep(5);
+    }
+
+    public function testFormInvalidUsername()
+    {
+        $this->url('http://romainlambot.fr/ninjav2/user/subscribe');
+        $this->byId('email')->value('lambot.rom14010@gmail.com');
+        $this->byId('username')->value('GoRFy');
+        $this->byCssSelector('.form_subscription')->submit();
+        $text = $this->byId("errorMsg")->text();
+        $this->assertEquals("Pseudonyme deja existant", $text, "Pseudonyme deja existant");
+        sleep(5);
+    }
+
+    public function testFormInvalidEmailAndUsername()
     {
         $this->url('http://romainlambot.fr/ninjav2/user/subscribe');
         $this->byId('email')->value('lambot.rom@gmail.com');
         $this->byId('username')->value('GoRFy');
         $this->byCssSelector('.form_subscription')->submit();
-        sleep(10);
+        $text = $this->byId("errorMsg")->text();
+        $this->assertEquals("Email incorrect ou deja existante
+Pseudonyme deja existant", $text, "Email incorrect ou deja existante
+Pseudonyme deja existant");
+        sleep(5);
     }
 
     public function tearDown()
